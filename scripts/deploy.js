7bf44dc0c34e19dc07ccc8948b3640b5cfc68e92
ArieFadz <fadz.arie@gmail.com>
2022-09-02 21:03:58 +0700

    First Commit

---
+++ scripts/deploy.js
@@ -0,0 +1,30 @@
+//  https://chiru-labs.github.io/ERC721A/#/upgradeable?id=deployment
+//  scripts/deploy.js
+const { ethers, upgrades } = require('hardhat');
+const fs = require('fs');
+
+async function main () {
+    const Something = await ethers.getContractFactory('Something');
+    console.log('Deploying...');
+    const something = await upgrades.deployProxy(
+        Something, 
+        [], 
+        { initializer: 'initialize' }
+    );
+    await something.deployed();
+    const addresses = {
+        proxy: something.address,
+        admin: await upgrades.erc1967.getAdminAddress(something.address), 
+        implementation: await upgrades.erc1967.getImplementationAddress(
+            something.address)
+    };
+    console.log('Addresses:', addresses);
+
+    try { 
+        await run('verify', { address: addresses.implementation });
+    } catch (e) {}
+
+    fs.writeFileSync('deployment-addresses.json', JSON.stringify(addresses));
+}
+
+main();
---
+++ scripts/upgrade.js
@@ -0,0 +1,28 @@
+//  https://chiru-labs.github.io/ERC721A/#/upgradeable?id=deployment
+//  scripts/upgrade.js
+const { ethers, upgrades } = require('hardhat');
+const fs = require('fs');
+
+async function main () {
+    const Something = await ethers.getContractFactory('Something');
+    console.log('Upgrading...');
+    let addresses = JSON.parse(fs.readFileSync('deployment-addresses.json'));
+    await upgrades.upgradeProxy(addresses.proxy, Something);
+    console.log('Upgraded');
+
+    addresses = {
+        proxy: addresses.proxy,
+        admin: await upgrades.erc1967.getAdminAddress(addresses.proxy), 
+        implementation: await upgrades.erc1967.getImplementationAddress(
+            addresses.proxy)
+    };
+    console.log('Addresses:', addresses);
+    
+    try { 
+        await run('verify', { address: addresses.implementation });
+    } catch (e) {}
+
+    fs.writeFileSync('deployment-addresses.json', JSON.stringify(addresses));
+}
+
+main();
