eb59ff7bcc4c410b151470a99bd4090e112ee87a
Arie Fadz <39182122+ArieFadz@users.noreply.github.com>
2022-09-03 22:38:58 +0700

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,32 +1,33 @@
-/**
- * // https://chiru-labs.github.io/ERC721A/#/upgradeable?id=local
- * // Local
- * // hardhat.config.js
- * require("@nomiclabs/hardhat-waffle");
- * require('@openzeppelin/hardhat-upgrades');
- * 
- * module.exports = {
- *  solidity: "0.8.11"
- *  };
- */
-
-// Testnet / Mainnet
+// https://chiru-labs.github.io/ERC721A/#/upgradeable?id=local
+// Local
+// hardhat.config.js
 require("@nomiclabs/hardhat-waffle");
-require('dotenv').config();
 require('@openzeppelin/hardhat-upgrades');
-require("@nomiclabs/hardhat-etherscan");
 
 module.exports = {
-    solidity: "0.8.11",
-    networks: {
-        goerli: {
-            url: process.env.RPC_URL_GOERLI,
-            accounts: [process.env.PRIVATE_KEY]
-        }
-    },
-    etherscan: {
-        // Your API key for Etherscan
-        // Obtain one at https://etherscan.io/
-        apiKey: process.env.ETHERSCAN_KEY
-    }
+    solidity: "0.8.11"
 };
+ 
+
+/**
+* // Testnet / Mainnet
+* require("@nomiclabs/hardhat-waffle");
+* require('dotenv').config();
+* require('@openzeppelin/hardhat-upgrades');
+* require("@nomiclabs/hardhat-etherscan");
+* 
+* module.exports = {
+*     solidity: "0.8.11",
+*     networks: {
+*         goerli: {
+*             url: process.env.RPC_URL_GOERLI,
+*             accounts: [process.env.PRIVATE_KEY]
+*         }
+*     },
+*     etherscan: {
+*         // Your API key for Etherscan
+*         // Obtain one at https://etherscan.io/
+*         apiKey: process.env.ETHERSCAN_KEY
+*     }
+* };
+*/
