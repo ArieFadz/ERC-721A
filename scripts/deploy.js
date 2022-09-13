4e444dd5c4b2d6609bcaa24fdf39203dc8d34ab8
Arie Fadz <39182122+ArieFadz@users.noreply.github.com>
2022-09-13 23:58:42 +0700

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,28 +1,32 @@
 //  https://chiru-labs.github.io/ERC721A/#/upgradeable?id=deployment
-//  scripts/deploy.js
+// scripts/deploy.js
 const { ethers, upgrades } = require('hardhat');
 const fs = require('fs');
 
 async function main () {
-    const NFTCollectible = await ethers.getContractFactory('NFTCollectible');
+    const MyNft = await ethers.getContractFactory('MyNft');
     console.log('Deploying...');
-    const nftcollectible = await upgrades.deployProxy(
-        NFTCollectible, 
+    const mynft = await upgrades.deployProxy(
+        MyNft, 
         [], 
         { initializer: 'initialize' }
     );
-    await nftcollectible.deployed();
+    await mynft.deployed();
     const addresses = {
-        proxy: nftcollectible.address,
-        admin: await upgrades.erc1967.getAdminAddress(nftcollectible.address), 
+        proxy: mynft.address,
+        admin: await upgrades.erc1967.getAdminAddress(mynft.address), 
         implementation: await upgrades.erc1967.getImplementationAddress(
-            nftcollectible.address)
+            mynft.address)
     };
     console.log('Addresses:', addresses);
+    //console.log('Addresses: ${result.addresses}')
 
     try { 
         await run('verify', { address: addresses.implementation });
     } catch (e) {}
+    //} catch (e) {
+    //    console.log(e.message)
+    //}
 
     fs.writeFileSync('deployment-addresses.json', JSON.stringify(addresses));
 }
