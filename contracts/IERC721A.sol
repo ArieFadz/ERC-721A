967dfc0b4b5164acc81bf586f3f02838995879ef
Arie Fadz <39182122+ArieFadz@users.noreply.github.com>
2022-09-13 23:49:29 +0700

    Update IERC721A.sol

--- contracts/IERC721A.sol
+++ contracts/IERC721A.sol
@@ -1,123 +1,13 @@
 // SPDX-License-Identifier: MIT
-// ERC721A Contracts v4.2.2
-// Creator: Chiru Labs
 
-pragma solidity ^0.8.4;
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/utils/introspection/ERC165.sol";
 
 /**
- * @dev Interface of ERC721A.
+ * @dev Required interface of an ERC721 compliant contract.
  */
-interface IERC721A {
-    /**
-     * The caller must own the token or be an approved operator.
-     */
-    error ApprovalCallerNotOwnerNorApproved();
-
-    /**
-     * The token does not exist.
-     */
-    error ApprovalQueryForNonexistentToken();
-
-    /**
-     * Cannot query the balance for the zero address.
-     */
-    error BalanceQueryForZeroAddress();
-
-    /**
-     * Cannot mint to the zero address.
-     */
-    error MintToZeroAddress();
-
-    /**
-     * The quantity of tokens minted must be more than zero.
-     */
-    error MintZeroQuantity();
-
-    /**
-     * The token does not exist.
-     */
-    error OwnerQueryForNonexistentToken();
-
-    /**
-     * The caller must own the token or be an approved operator.
-     */
-    error TransferCallerNotOwnerNorApproved();
-
-    /**
-     * The token must be owned by `from`.
-     */
-    error TransferFromIncorrectOwner();
-
-    /**
-     * Cannot safely transfer to a contract that does not implement the
-     * ERC721Receiver interface.
-     */
-    error TransferToNonERC721ReceiverImplementer();
-
-    /**
-     * Cannot transfer to the zero address.
-     */
-    error TransferToZeroAddress();
-
-    /**
-     * The token does not exist.
-     */
-    error URIQueryForNonexistentToken();
-
-    /**
-     * The `quantity` minted with ERC2309 exceeds the safety limit.
-     */
-    error MintERC2309QuantityExceedsLimit();
-
-    /**
-     * The `extraData` cannot be set on an unintialized ownership slot.
-     */
-    error OwnershipNotInitializedForExtraData();
-
-    // =============================================================
-    //                            STRUCTS
-    // =============================================================
-
-    struct TokenOwnership {
-        // The address of the owner.
-        address addr;
-        // Stores the start time of ownership with minimal overhead for tokenomics.
-        uint64 startTimestamp;
-        // Whether the token has been burned.
-        bool burned;
-        // Arbitrary data similar to `startTimestamp` that can be set via {_extraData}.
-        uint24 extraData;
-    }
-
-    // =============================================================
-    //                         TOKEN COUNTERS
-    // =============================================================
-
-    /**
-     * @dev Returns the total number of tokens in existence.
-     * Burned tokens will reduce the count.
-     * To get the total number of tokens minted, please see {_totalMinted}.
-     */
-    function totalSupply() external view returns (uint256);
-
-    // =============================================================
-    //                            IERC165
-    // =============================================================
-
-    /**
-     * @dev Returns true if this contract implements the interface defined by
-     * `interfaceId`. See the corresponding
-     * [EIP section](https://eips.ethereum.org/EIPS/eip-165#how-interfaces-are-identified)
-     * to learn more about how these ids are created.
-     *
-     * This function call must use less than 30000 gas.
-     */
-    function supportsInterface(bytes4 interfaceId) external view returns (bool);
-
-    // =============================================================
-    //                            IERC721
-    // =============================================================
-
+interface IERC721 is IERC165 {
     /**
      * @dev Emitted when `tokenId` token is transferred from `from` to `to`.
      */
@@ -129,13 +19,12 @@ interface IERC721A {
     event Approval(address indexed owner, address indexed approved, uint256 indexed tokenId);
 
     /**
-     * @dev Emitted when `owner` enables or disables
-     * (`approved`) `operator` to manage all of its assets.
+     * @dev Emitted when `owner` enables or disables (`approved`) `operator` to manage all of its assets.
      */
     event ApprovalForAll(address indexed owner, address indexed operator, bool approved);
 
     /**
-     * @dev Returns the number of tokens in `owner`'s account.
+     * @dev Returns the number of tokens in ``owner``'s account.
      */
     function balanceOf(address owner) external view returns (uint256 balance);
 
@@ -149,31 +38,18 @@ interface IERC721A {
     function ownerOf(uint256 tokenId) external view returns (address owner);
 
     /**
-     * @dev Safely transfers `tokenId` token from `from` to `to`,
-     * checking first that contract recipients are aware of the ERC721 protocol
-     * to prevent tokens from being forever locked.
+     * @dev Safely transfers `tokenId` token from `from` to `to`, checking first that contract recipients
+     * are aware of the ERC721 protocol to prevent tokens from being forever locked.
      *
      * Requirements:
      *
      * - `from` cannot be the zero address.
      * - `to` cannot be the zero address.
      * - `tokenId` token must exist and be owned by `from`.
-     * - If the caller is not `from`, it must be have been allowed to move
-     * this token by either {approve} or {setApprovalForAll}.
-     * - If `to` refers to a smart contract, it must implement
-     * {IERC721Receiver-onERC721Received}, which is called upon a safe transfer.
+     * - If the caller is not `from`, it must be have been allowed to move this token by either {approve} or {setApprovalForAll}.
+     * - If `to` refers to a smart contract, it must implement {IERC721Receiver-onERC721Received}, which is called upon a safe transfer.
      *
      * Emits a {Transfer} event.
-     */
-    function safeTransferFrom(
-        address from,
-        address to,
-        uint256 tokenId,
-        bytes calldata data
-    ) external;
-
-    /**
-     * @dev Equivalent to `safeTransferFrom(from, to, tokenId, '')`.
      */
     function safeTransferFrom(
         address from,
@@ -182,18 +58,16 @@ interface IERC721A {
     ) external;
 
     /**
-     * @dev Transfers `tokenId` from `from` to `to`.
+     * @dev Transfers `tokenId` token from `from` to `to`.
      *
-     * WARNING: Usage of this method is discouraged, use {safeTransferFrom}
-     * whenever possible.
+     * WARNING: Usage of this method is discouraged, use {safeTransferFrom} whenever possible.
      *
      * Requirements:
      *
      * - `from` cannot be the zero address.
      * - `to` cannot be the zero address.
      * - `tokenId` token must be owned by `from`.
-     * - If the caller is not `from`, it must be approved to move this token
-     * by either {approve} or {setApprovalForAll}.
+     * - If the caller is not `from`, it must be approved to move this token by either {approve} or {setApprovalForAll}.
      *
      * Emits a {Transfer} event.
      */
@@ -207,8 +81,7 @@ interface IERC721A {
      * @dev Gives permission to `to` to transfer `tokenId` token to another account.
      * The approval is cleared when the token is transferred.
      *
-     * Only a single account can be approved at a time, so approving the
-     * zero address clears previous approvals.
+     * Only a single account can be approved at a time, so approving the zero address clears previous approvals.
      *
      * Requirements:
      *
@@ -220,9 +93,17 @@ interface IERC721A {
     function approve(address to, uint256 tokenId) external;
 
     /**
+     * @dev Returns the account approved for `tokenId` token.
+     *
+     * Requirements:
+     *
+     * - `tokenId` must exist.
+     */
+    function getApproved(uint256 tokenId) external view returns (address operator);
+
+    /**
      * @dev Approve or remove `operator` as an operator for the caller.
-     * Operators can call {transferFrom} or {safeTransferFrom}
-     * for any token owned by the caller.
+     * Operators can call {transferFrom} or {safeTransferFrom} for any token owned by the caller.
      *
      * Requirements:
      *
@@ -233,50 +114,29 @@ interface IERC721A {
     function setApprovalForAll(address operator, bool _approved) external;
 
     /**
-     * @dev Returns the account approved for `tokenId` token.
-     *
-     * Requirements:
-     *
-     * - `tokenId` must exist.
-     */
-    function getApproved(uint256 tokenId) external view returns (address operator);
-
-    /**
      * @dev Returns if the `operator` is allowed to manage all of the assets of `owner`.
      *
-     * See {setApprovalForAll}.
+     * See {setApprovalForAll}
      */
     function isApprovedForAll(address owner, address operator) external view returns (bool);
 
-    // =============================================================
-    //                        IERC721Metadata
-    // =============================================================
-
     /**
-     * @dev Returns the token collection name.
-     */
-    function name() external view returns (string memory);
-
-    /**
-     * @dev Returns the token collection symbol.
-     */
-    function symbol() external view returns (string memory);
-
-    /**
-     * @dev Returns the Uniform Resource Identifier (URI) for `tokenId` token.
-     */
-    function tokenURI(uint256 tokenId) external view returns (string memory);
-
-    // =============================================================
-    //                           IERC2309
-    // =============================================================
-
-    /**
-     * @dev Emitted when tokens in `fromTokenId` to `toTokenId`
-     * (inclusive) is transferred from `from` to `to`, as defined in the
-     * [ERC2309](https://eips.ethereum.org/EIPS/eip-2309) standard.
+     * @dev Safely transfers `tokenId` token from `from` to `to`.
      *
-     * See {_mintERC2309} for more details.
+     * Requirements:
+     *
+     * - `from` cannot be the zero address.
+     * - `to` cannot be the zero address.
+     * - `tokenId` token must exist and be owned by `from`.
+     * - If the caller is not `from`, it must be approved to move this token by either {approve} or {setApprovalForAll}.
+     * - If `to` refers to a smart contract, it must implement {IERC721Receiver-onERC721Received}, which is called upon a safe transfer.
+     *
+     * Emits a {Transfer} event.
      */
-    event ConsecutiveTransfer(uint256 indexed fromTokenId, uint256 toTokenId, address indexed from, address indexed to);
+    function safeTransferFrom(
+        address from,
+        address to,
+        uint256 tokenId,
+        bytes calldata data
+    ) external;
 }
